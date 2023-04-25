d98fb88e876da2792e6545d04794ede74ba4b5fd
Sanguk Lee <az0t0@naver.com>
2023-04-25 16:54:05 +0900

    Initial commit

---
+++ src/main/java/com/hgdroplet/unoserver/UnoserverApplication.java
@@ -0,0 +1,13 @@
+package com.hgdroplet.unoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnoserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hgdroplet/unoserver/UnoserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgdroplet.unoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
